fa9c48c6ca58e3c20fd0530183b3ee498748bbe5
esa fauzi <esa fauzi@gmail.com>
2024-03-26 14:20:13 +0700

    add: mahasiswa class

--- src/components/HaloBambang.vue
+++ src/components/HaloBambang.vue
@@ -1,15 +1,21 @@
 <template>
-  <h1>Hallo oi bambang</h1><br>
-  <h1>Umurmu segini bukan? {{ count }}</h1><br>
-  <button @click="increment">Count</button>
+  <p>hello apa kabar</p><br>
+  <v-text-field v-model="mhs.npm" label="NPM" variant="outlined"></v-text-field><br>
+  <v-text-field v-model="mhs.nama" label="Nama" variant="outlined"></v-text-field><br>
+  <v-text-field v-model="mhs.ipk" label="IPK" variant="outlined"></v-text-field><br>
+  <v-btn @click="createMahasiswa" color="#AB47BC">
+    Submit
+  </v-btn><br>
 </template>
-<script>
+<script lang="ts">
+import { Mahasiswa } from '@/model/Mahasiswa.ts'
 export default {
   // Properties returned from data() become reactive state
   // and will be exposed on `this`.
   data() {
     return {
-      count: 0
+      count: 100,
+      mhs: new Mahasiswa ('1234', 'Bambang', 3.4)
     }
   },
 
@@ -18,6 +24,9 @@ export default {
   methods: {
     increment() {
       this.count++
+    },
+    createMahasiswa() {
+      alert('Mahasiswa '+ this.mhs.nama +' berhasil dibuat')
     }
   },
 
