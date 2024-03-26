a93baa2c8946c847c13c59716491684b6707aaf9
esa fauzi <esa fauzi@gmail.com>
2024-03-26 17:24:30 +0700

    add: Mahasiswa form

---
+++ src/components/MahasiswaForm.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-text-field label="NPM"
+        variant="outlined" v-model="mhs.npm">
+      </v-text-field>
+    </v-row>
+    <v-row>
+      <v-text-field label="Nama" 
+      v-model="mhs.nama" variant="outlined"></v-text-field>
+    </v-row>
+    <v-row>
+      <v-text-field label="IPK"
+      v-model="mhs.ipk" variant="outlined"></v-text-field>
+    </v-row>
+    <v-row>
+      <v-btn color="blue" @click="submitMahasiswa">SUBMIT</v-btn>
+    </v-row>
+  </v-container>
+</template>
+<script lang="ts">
+import { Mahasiswa } from '@/model/Mahasiswa'
+export default {
+  data() {
+    return {
+      mhs: new Mahasiswa ('', '', 0)
+    }
+  },
+  methods:{
+    submitMahasiswa(){
+      alert('Data mahasiswa ' + this.mhs.nama + ' berhasil disimpan')
+    }
+  }
+}
+</script>
--- src/model/Mahasiswa.ts
+++ src/model/Mahasiswa.ts
@@ -1,9 +1,9 @@
 export class Mahasiswa {
-  public npm:number
+  public npm:string | null
   public nama:string
   public ipk:number
 
-  constructor(npm: number, nama: string, ipk: number) {
+  constructor(npm: string, nama: string, ipk: number) {
     this.npm = npm
     this.nama = nama
     this.ipk = ipk
